1098de08f6cc1b5d4a3b7193d86d05286a4f9742
Gigi M <mmgamao1@gmail.com>
2019-03-03 17:01:04 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,14 +1,8 @@
-languages = ['ruby', 'javascript', 'python', 'objective-c'] 
-
-def my_collect(languages) 
-  i = 0  
-  while i < language.length  
-  puts language.upcase
-end
 
 
 
-end
+
+
 
 def my_collect(collection) 
   empty_arr = [] 
